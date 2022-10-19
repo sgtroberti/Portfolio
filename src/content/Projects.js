ea4552f76ef0635dd1f9946938cc2da63cda53a9
Guilherme Roberti <guimourinha@gmail.com>
2022-10-19 11:10:33 -0300

    final commit

--- src/content/Projects.js
+++ src/content/Projects.js
@@ -1,4 +1,7 @@
-import placeholder from "./images/test.png";
+import gotImage from "./images/got.jpg";
+import petwitter from "./images/pet.png";
+import rancho from "./images/rancho.png";
+import gda from "./images/gda.png";
 
 export const Projects = [
   {
@@ -7,17 +10,39 @@ export const Projects = [
     description:
       "Sistema de entrada e saída para organizações militares controlado por QR Code [...]",
     stackTree: "React | Node.js | Prisma | PostgreSql",
-    image: placeholder,
+    image: gda,
     longDescription: `Sistema de controle de entrada e saída para organizações militares. Controla a entrada e saída de pessoal cadastrado (via QR Code), pessoal não cadastrado (cadastro rápido feito na hora via celular) e controle de hodômetros de viaturas em missões.`,
     date: `Desenvolvido e implementado em 2022`,
+    video: "https://www.youtube.com/watch?v=1aJjmSiPWY4",
   },
   {
     id: 2,
+    name: "PeTwitter [POC]",
+    description: "Clone do Twitter para pets [...]",
+    stackTree: "React | Node.js | Prisma | PostgreSql",
+    image: petwitter,
+    longDescription: `Clone do Twitter para pets, projeto criado para desenvolvimento de habilidades.`,
+    date: `Desenvolvido em 2021/2022`,
+    github: "https://github.com/sgtroberti/petwitter-front",
+  },
+  {
+    id: 3,
+    name: "Game Of Thrones Books - API [POC]",
+    description: "API de livros de Game Of Thrones [...]",
+    stackTree: "Node.js | MongoDB",
+    image: gotImage,
+    longDescription: `API de livros de Game Of Thrones, projeto criado para desenvolvimento de habilidades.`,
+    date: `Desenvolvido em 2021/2022`,
+    github: "https://github.com/sgtroberti/jobs/tree/master/backend",
+  },
+  {
+    id: 4,
     name: "Arranchamento Online",
     description: "Agendamento de refeições para organizações militares [...]",
     stackTree: "HTML | CSS | BootStrap | PHP | MySql",
-    image: placeholder,
+    image: rancho,
     longDescription: `Sistema utilizado para agendamento de refeições para funcionários, a fim de evitar desperdícios para cozinhas industriais.`,
     date: `Desenvolvido e implementado em 2019`,
+    video: "https://www.youtube.com/watch?v=6u5_3lldZVA",
   },
 ];
--- src/routes/ProjectDetail.js
+++ src/routes/ProjectDetail.js
@@ -1,9 +1,11 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, Link, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import Contact from "../components/Contact";
 import Navbar from "../components/Navbar";
 import Wrapper from "../components/Wrapper";
 import { Projects } from "../content/Projects";
+
+import ReactPlayer from "react-player";
 
 const ProjectDetail = () => {
   const { id } = useParams();
@@ -28,16 +30,44 @@ const ProjectDetail = () => {
     <>
       <Navbar notHome />
       <Wrapper>
-        <Flex flexDir={"column"} gap={2}>
-          <Text fontSize={["28px", "48px"]} fontWeight={700}>
-            {productDetail.name}
-          </Text>
-          <Text fontSize={["16px", "24px"]} color={"#929292"}>
-            {productDetail.stackTree}
-          </Text>
-          <Text w={["100%", "50%"]}>{productDetail.longDescription}</Text>
-          <Text w={["100%", "50%"]}>{productDetail.date}</Text>
-          <Image src={productDetail.image} />
+        <Flex
+          flexDir={["column", "row"]}
+          alignItems={"center"}
+          justifyContent={"space-evenly"}
+          w="100%"
+          gap={2}
+        >
+          <Flex
+            flexDir={"column"}
+            gap={2}
+            justifyContent={"space-evenly"}
+            w="100%"
+          >
+            <Text fontSize={["28px", "48px"]} fontWeight={700}>
+              {productDetail.name}
+            </Text>
+            <Text fontSize={["16px", "24px"]} color={"#929292"}>
+              {productDetail.stackTree}
+            </Text>
+            <Text w={["100%", "80%"]}>{productDetail.longDescription}</Text>
+            <Text w={["100%", "80%"]}>{productDetail.date}</Text>
+            {!productDetail?.github ? (
+              <Text>Github não disponível</Text>
+            ) : (
+              <Link href={productDetail.github} target="_blank">
+                Ver no Github
+              </Link>
+            )}
+          </Flex>
+          {!productDetail?.video ? (
+            <Image borderRadius={16} src={productDetail.image} />
+          ) : (
+            <ReactPlayer
+              width={"100%"}
+              height={"600px"}
+              url={productDetail.video}
+            />
+          )}
         </Flex>
       </Wrapper>
       <Contact />
